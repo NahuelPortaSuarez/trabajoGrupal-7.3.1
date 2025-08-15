64fe16fecf206ddfbf28a5ead688e041e0227a4f
melaniefrade <melanie.frade@estudiantes.utec.edu.uy>
2025-08-14 21:22:21 -0300

    Alerta de exito y error

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,47 @@
 function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
+  const alerta = document.getElementById("alert-success");
+  alerta.classList.add("show");
+
+  setTimeout(() => {
+    alerta.classList.remove("show");
+  }, 3000);
 }
 
 function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+  const alerta = document.getElementById("alert-danger");
+  alerta.classList.add("show");
+
+  setTimeout(() => {
+    alerta.classList.remove("show");
+  }, 3000);
+}
+
+document.getElementById("regBtn").addEventListener("click", function () {
+  validarFormulario();
+});
+
+function validarFormulario() {
+  let nombre = document.getElementById("nombre").value.trim();
+  let apellido = document.getElementById("apellido").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let pass1 = document.getElementById("password1").value.trim();
+  let pass2 = document.getElementById("password2").value.trim();
+  let terminos = document.getElementById("terminos").checked;
+
+  if (!nombre || !apellido || !email || !pass1 || !pass2 || !terminos) {
+    showAlertError();
+    return false;
+  }
+
+  if (pass1.length < 6 || pass1 !== pass2) {
+    showAlertError();
+    return false;
+  }
+  if (!terminos) {
+    showAlertError("Debes aceptar los términos y condiciones");
+    return;
+  }
+
+  showAlertSuccess();
+  return true;
 }
